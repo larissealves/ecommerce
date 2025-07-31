636399ffb9cd424235c3f34fe5e7bb6971f7dec2
larissealves <alves.larisser@gmail.com>
2025-07-31 17:35:21 -0300

    adc `filtro por nome da equipe`

--- training-api-consumption/backend/src/server.js
+++ training-api-consumption/backend/src/server.js
@@ -5,7 +5,8 @@ import routes from "./services/routes/route.js";
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    credentials: true
   })
 );
 
