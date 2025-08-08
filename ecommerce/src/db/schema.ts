2a4c521a75c84ebdf150dfc761327db9af53eba8
larissealves <alves.larisser@gmail.com>
2025-08-07 23:21:24 -0300

    chore: add better-auth tables

--- ecommerce/src/db/schema.ts
+++ ecommerce/src/db/schema.ts
@@ -1,12 +1,74 @@
-import { pgTable, uuid,  text, integer, boolean, timestamp } from "drizzle-orm/pg-core";
+import {
+  pgTable,
+  uuid,
+  text,
+  integer,
+  boolean,
+  timestamp,
+} from "drizzle-orm/pg-core";
 import { relations } from "drizzle-orm";
 
-export const userTable = pgTable('user', {
-  id: uuid().primaryKey().defaultRandom(),
-  name: text().notNull(),
+export const userTable = pgTable("user_table", {
+  id: text("id").primaryKey(),
+  name: text("name").notNull(),
+  email: text("email").notNull().unique(),
+  emailVerified: boolean("email_verified")
+    .$defaultFn(() => false)
+    .notNull(),
+  image: text("image"),
+  createdAt: timestamp("created_at")
+    .$defaultFn(() => /* @__PURE__ */ new Date())
+    .notNull(),
+  updatedAt: timestamp("updated_at")
+    .$defaultFn(() => /* @__PURE__ */ new Date())
+    .notNull(),
 });
 
-export const categoryTable = pgTable('category', {
+export const sessionTable = pgTable("session_table", {
+  id: text("id").primaryKey(),
+  expiresAt: timestamp("expires_at").notNull(),
+  token: text("token").notNull().unique(),
+  createdAt: timestamp("created_at").notNull(),
+  updatedAt: timestamp("updated_at").notNull(),
+  ipAddress: text("ip_address"),
+  userAgent: text("user_agent"),
+  userId: text("user_id")
+    .notNull()
+    .references(() => userTable.id, { onDelete: "cascade" }),
+});
+
+export const accountTable = pgTable("account_table", {
+  id: text("id").primaryKey(),
+  accountId: text("account_id").notNull(),
+  providerId: text("provider_id").notNull(),
+  userId: text("user_id")
+    .notNull()
+    .references(() => userTable.id, { onDelete: "cascade" }),
+  accessToken: text("access_token"),
+  refreshToken: text("refresh_token"),
+  idToken: text("id_token"),
+  accessTokenExpiresAt: timestamp("access_token_expires_at"),
+  refreshTokenExpiresAt: timestamp("refresh_token_expires_at"),
+  scope: text("scope"),
+  password: text("password"),
+  createdAt: timestamp("created_at").notNull(),
+  updatedAt: timestamp("updated_at").notNull(),
+});
+
+export const verificationTable = pgTable("verification_table", {
+  id: text("id").primaryKey(),
+  identifier: text("identifier").notNull(),
+  value: text("value").notNull(),
+  expiresAt: timestamp("expires_at").notNull(),
+  createdAt: timestamp("created_at").$defaultFn(
+    () => /* @__PURE__ */ new Date(),
+  ),
+  updatedAt: timestamp("updated_at").$defaultFn(
+    () => /* @__PURE__ */ new Date(),
+  ),
+});
+
+export const categoryTable = pgTable("category", {
   id: uuid().primaryKey().defaultRandom(),
   categoryName: text("category_name").notNull().unique(),
   slug: text().unique(),
@@ -14,13 +76,15 @@ export const categoryTable = pgTable('category', {
   isActive: boolean("is_active"),
 });
 
-export const categoryRelations = relations(categoryTable, ({many}) => ({
-    products: many(productTable),
+export const categoryRelations = relations(categoryTable, ({ many }) => ({
+  products: many(productTable),
 }));
 
-export const productTable = pgTable('product', {
+export const productTable = pgTable("product", {
   id: uuid().primaryKey().defaultRandom(),
-  categoryId: uuid("category_id").notNull().references(() => categoryTable.id),
+  categoryId: uuid("category_id")
+    .notNull()
+    .references(() => categoryTable.id),
   productName: text("product_name").notNull(),
   slug: text().notNull().unique(),
   description: text(),
@@ -30,25 +94,27 @@ export const productTable = pgTable('product', {
 
 export const productVariantTable = pgTable("product_variant", {
   id: uuid().primaryKey().defaultRandom(),
-  productId: uuid('product_id').notNull().references (()=> productTable.id),
+  productId: uuid("product_id")
+    .notNull()
+    .references(() => productTable.id),
   priceInCents: integer("price_in_cents").notNull(),
   //R$ 10,00 --> save with: 1000
   //conversion: 1000/100 => 10
   createdAt: timestamp("created_at").notNull().defaultNow(),
   slug: text().notNull().unique(),
   name: text().notNull(),
-  imageUrl: text('image_url').notNull(),
-  // salvar imagem em serviço de armazenamento de imagens estáticas, 
+  imageUrl: text("image_url").notNull(),
+  // salvar imagem em serviço de armazenamento de imagens estáticas,
   // pega o url e salva no BD interno.
   color: text().notNull(),
-})
+});
 
-export const productRelations = relations(productTable, ({one, many}) => ({
-    category: one(categoryTable, {
-      fields: [productTable.categoryId],
-      references: [categoryTable.id],
-    }),
-    variants: many(productVariantTable),
+export const productRelations = relations(productTable, ({ one, many }) => ({
+  category: one(categoryTable, {
+    fields: [productTable.categoryId],
+    references: [categoryTable.id],
+  }),
+  variants: many(productVariantTable),
 }));
 
 export const productVariantRelations = relations(
@@ -60,4 +126,3 @@ export const productVariantRelations = relations(
     }),
   }),
 );
-
